480ae034ba62e1d7a5d2d9cfa86468e0f405fcab
WhiteSlash <122911847+WhiteSlashDev@users.noreply.github.com>
2023-01-17 23:05:11 +0500

    Update password_check.php

--- password_check.php
+++ password_check.php
@@ -2,7 +2,8 @@
     if(isset($_POST['password'])){
         $password = $_POST['password'];
         if($password === "123"){
-            header("Location: page.html");
+            $url = 'https://whiteslashdev.github.io/page.html';
+            header("Location: $url");
             exit();
         }else{
             echo "Incorrect password. Please try again.";
